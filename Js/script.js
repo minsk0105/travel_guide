e9fa52cb20545331a41e664011eb9c9879fe8eda
minsk0105 <cminseog7065@gmail.com>
2025-04-14 18:07:41 +0900

    .

--- Js/script.js
+++ Js/script.js
@@ -1,17 +1,21 @@
-// 헤더
-const header = document.querySelector("header");
-let currentY = window.scrollY;
+// header
+const header = document.querySelector('header');
+let currentY = 150;
 
 window.addEventListener('scroll', () => {
-    if (currentY < this.window.scrollY) {
+    const thisScr = this.window.scrollY;
+    if (150 < thisScr && currentY < thisScr) {
+        header.classList.add("scrolled");
         header.style.transform = 'translateY(-100%)';
-        header.classList.add("scrolling");
-        currentY = this.window.scrollY;
-    } else {
-        header.style.transform = 'translateY(0)';
-        currentY = this.window.scrollY;
-    }
-    if (this.window.scrollY === 0) {
-        header.classList.remove("scrolling");
-    }
+    } else header.style.transform = 'translateY(0)';
+
+    currentY = thisScr;
+    if (thisScr === 0) header.classList.remove("scrolled");
+
+    // visual
+    const visual = document.querySelector('.main');
+    if (thisScr > 120) {
+        let radius = thisScr / 10;
+        visual.style.borderRadius = radius + "px";
+    } else visual.style.borderRadius = 0;
 });
