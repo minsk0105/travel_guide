95df3b119a62c90a65d6f268f0ef10a031db7ae4
minsk0105 <cminseog7065@gmail.com>
2025-04-17 12:22:51 +0900

    .

--- Js/app.js
+++ Js/app.js
@@ -1,11 +1,14 @@
 $(window).on('scroll', () => {
+
     const currentY = this.window.scrollY;
     const headerState = (currentY !== 0)
-        ? $('header').addClass('not_top')
-        : $('header').removeClass('not_top');
-    $('#visual').css('background-color', `rgba(0, 0, 0, ${(0.3 + currentY / 1100)})`);
-    $('.cate_box').css('opacity', (currentY / 400 - .9));
+        ? $('header').addClass('scrolled')
+        : $('header').removeClass('scrolled');
+    
+    $('#visual').css('background-color', `rgba(0, 0, 0, ${(currentY / 1000) + .2})`);
 
-    if (currentY >= 650) $('.category').addClass('show');
-    else $('.category').removeClass('show');
+    const visualState = (currentY >= 650)
+        ? $('.category').addClass('show')
+        : $('.category').removeClass('show');
+
 });
