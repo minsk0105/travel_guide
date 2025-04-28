bf4009fb159da8edf45791ed2e535136318e7364
minsk0105 <cminseog7065@gmail.com>
2025-04-28 20:50:16 +0900

    .

--- Js/app.js
+++ Js/app.js
@@ -16,28 +16,57 @@ $(window).on('scroll', () => {
 const swiper = document.querySelector('.swiper');
 const boxes = document.querySelectorAll('.attr_box');
 
-let page = -700;
+let page = -1400;
 let total = boxes.length - 1;
-let index = 1;
+let index = 2;
 
 const swipBtns = document.querySelectorAll('.swip_btn button');
-const sweep = (boxes) => {
-    swipBtns.forEach((btn) => {
-        btn.addEventListener('click', () => {
-            if (btn.classList.contains("next")) {
-                page -= 700;
-                index += 1;
-            } else {
-                page += 700;
-                index -= 1;
+
+swipBtns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        if (btn.classList.contains("next")) {
+            page -= 700;
+            index += 1;
+        } else {
+            page += 700;
+            index -= 1;
+        }
+
+        if (page === -700) {
+            setTimeout(() => {
+                index = 5;
+                page = -3500;
+                swiper.style.transition = '0s';
+                swiper.style.transform = 'translateX(-3500px)';
+            }, 600);
+        } else if (page === -4200) {
+            setTimeout(() => {
+                index = 2;
+                page = -1400;
+                swiper.style.transition = '0s';
+                swiper.style.transform = 'translateX(-1400px)';
+            }, 600);
+        }
+        swiper.style.transition = '0.6s ease-in-out';
+
+        boxes.forEach((item, num) => {
+            if (index === num) {
+                const sameSrc = item.children[0].children[0].src;
+                boxes.forEach((box) => {
+                    if (sameSrc === box.children[0].children[0].src) {
+                        box.children[0].classList.add('curr_img');
+                    } else box.children[0].classList.remove('curr_img');
+                });
             }
-            boxes.forEach((item, num) => {
-                const currBox = (index === num)
-                    ? item.children[0].classList.add("curr_img")
-                    : item.children[0].classList.remove("curr_img");
-            });
-            swiper.style.transform = `translateX(${page}px)`;
         });
+
+        swipBtns.forEach((btn) => {
+            btn.style.pointerEvents = 'none';
+            setTimeout(() => {
+                btn.style.pointerEvents = 'auto';
+            }, 600);
+        });
+
+        swiper.style.transform = `translateX(${page}px)`;
     });
-};
-sweep(boxes);
+});
