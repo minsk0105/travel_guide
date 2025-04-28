cabffe206cfc2721a1fcdbe48a968bd9190ab75a
minsk0105 <cminseog7065@gmail.com>
2025-04-28 18:02:45 +0900

    Add Attractions

--- Js/app.js
+++ Js/app.js
@@ -7,8 +7,37 @@ $(window).on('scroll', () => {
     
     $('#visual').css('background-color', `rgba(0, 0, 0, ${(currentY / 1000) + .2})`);
 
-    const visualState = (currentY >= 650)
-        ? $('.category').addClass('show')
-        : $('.category').removeClass('show');
+    // const visualState = (currentY >= 650)
+    //     ? $('.category').addClass('show')
+    //     : $('.category').removeClass('show');
 
 });
+
+const swiper = document.querySelector('.swiper');
+const boxes = document.querySelectorAll('.attr_box');
+
+let page = -700;
+let total = boxes.length - 1;
+let index = 1;
+
+const swipBtns = document.querySelectorAll('.swip_btn button');
+const sweep = (boxes) => {
+    swipBtns.forEach((btn) => {
+        btn.addEventListener('click', () => {
+            if (btn.classList.contains("next")) {
+                page -= 700;
+                index += 1;
+            } else {
+                page += 700;
+                index -= 1;
+            }
+            boxes.forEach((item, num) => {
+                const currBox = (index === num)
+                    ? item.children[0].classList.add("curr_img")
+                    : item.children[0].classList.remove("curr_img");
+            });
+            swiper.style.transform = `translateX(${page}px)`;
+        });
+    });
+};
+sweep(boxes);
